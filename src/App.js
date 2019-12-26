526e7e349fe16bde9d7f76fad69b88b6270745f1
Shridhar Rai <shridharrai3@gmail.com>
2019-12-26 09:15:48 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Fragment, useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
-import './App.css';
 import { SearchBar } from './components/layout/SearchBar';
 import { AddBtn } from './components/layout/AddBtn';
 import { AddLogModal } from './components/logs/AddLogModal';
